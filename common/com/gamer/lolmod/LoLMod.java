e24d541868072e3ee9f3c1eb9c1f700603ff9710
ImTheGamer <atillacsa@live.com>
2013-04-19 14:38:45 +0200

    2013.04.19.

---
+++ common/com/gamer/lolmod/LoLMod.java
@@ -0,0 +1,28 @@
+package com.gamer.lolmod;
+
+import com.gamer.lolmod.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, 
+     name = Reference.MOD_NAME, 
+     version = Reference.Version)
+
+public class LoLMod {
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event){
+        
+    }
+    @Init
+    public void init(FMLPreInitializationEvent event){
+        
+    }
+    @PostInit
+    public void postInit(FMLPreInitializationEvent event){
+        
+    }
+}
---
+++ common/com/gamer/lolmod/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.gamer.lolmod.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "LoLMod";
+    public static final String MOD_NAME ="LoL Mod";
+    public static final String Version = "0.0.1";
+}
